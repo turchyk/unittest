c6890dccb81930e7fcd605dc2fcd4cd106ad5278
Zakhar Turchyk <turchikz20@gmail.com>
2021-04-09 13:09:40 +0300

    Switching to locators in the locators class

--- tests/test_one.py
+++ tests/test_one.py
@@ -1,8 +1,6 @@
 import unittest
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from locators import Common, Main, Product, Cart, User
 
 
 class TestCase(unittest.TestCase):
@@ -19,41 +17,44 @@ class TestCase(unittest.TestCase):
         browser = self.driver
         browser.get('http://opencart/')
         # Click on the first element in the block featured
-        feature_product = browser.find_elements_by_css_selector("#content > div.row .product-layout")[0]
-        product_name = feature_product.find_element_by_css_selector(".caption h4 a").text
+        feature_product = browser.find_elements_by_css_selector(Main.featured.products['css'])[0]
+        product_name = feature_product.find_element_by_css_selector(Main.featured.names['css']).text
         feature_product.click()
         # Click on the button on the Product page
-        browser.find_element_by_css_selector("[data-original-title='Add to Wish List']").click()
+        browser.find_element_by_css_selector(Product.add_to_wishlist['css']).click()
         # Click on the link in the alert-success block
-        browser.find_element_by_css_selector(".alert-success").find_element_by_link_text('login').click()
+        browser.find_element_by_css_selector(Common.alert.success.login['css']).click()
         # Login from the user authorization form
-        browser.find_element_by_css_selector("#input-email").send_keys("turchikz@gmail.com")
-        browser.find_element_by_css_selector("#input-password").send_keys("0000")
-        browser.find_element_by_css_selector("input[value=Login]").click()
+        browser.find_element_by_css_selector(Common.user_login.email_input['css']).send_keys("test2@mail.ru")
+        browser.find_element_by_css_selector(Common.user_login.password_input['css']).send_keys("test")
+        browser.find_element_by_css_selector(Common.user_login.login_button['css']).click()
         # Go to favorites section
-        browser.find_element_by_xpath("//*[@id='column-right']//*[text()='Wish List']").click()
+        browser.find_element_by_css_selector(User.right_menu.wish_list['css']).click()
         # Checking the link with the text of the selected product
-        WebDriverWait(browser, 5).until(EC.visibility_of_element_located((By.LINK_TEXT, product_name)))
+        browser.find_element_by_link_text(product_name)
 
     def test_add_to_cart(self):
         browser = self.driver
         browser.get('http://opencart/')
         # Click on the first element in the block featured
-        feature_product = browser.find_elements_by_css_selector("#content > div.row .product-layout")[0]
-        product_name = feature_product.find_element_by_css_selector(".caption h4 a").text
+        feature_product = browser.find_elements_by_css_selector(Main.featured.products['css'])[0]
+        product_name = feature_product.find_element_by_css_selector(Main.featured.names['css']).text
         feature_product.click()
         # Click on the button on the Product page
-        browser.find_element_by_css_selector("#button-cart").click()
+        browser.find_element_by_css_selector(Product.add_to_cart['css']).click()
         # Click on the link in the alert-success block
-        browser.find_element_by_css_selector(".alert-success").find_element_by_link_text("shopping cart").click()
-        WebDriverWait(browser, 5).until(EC.visibility_of_element_located((By.LINK_TEXT, product_name)))
+        browser.find_element_by_css_selector(Common.alert.success.to_cart['css']).click()
+        # Checking the link with the text of the selected product
+        browser.find_element_by_link_text(product_name)
+        # Click on the Checkout button on the cart page
+        browser.find_element_by_css_selector(Cart.bottom_btn.checkout['css']).click()
         # Login from the user authorization form
-        browser.find_element_by_css_selector(".buttons").find_element_by_link_text("Checkout").click()
-        browser.find_element_by_css_selector("#input-password").send_keys("0000")
-        browser.find_element_by_css_selector("#input-email").send_keys("turchikz@gmail.com")
-        browser.find_element_by_css_selector("input[value=Login]").click()
+        browser.find_element_by_css_selector(Common.user_login.email_input['css']).send_keys("test2@mail.ru")
+        browser.find_element_by_css_selector(Common.user_login.password_input['css']).send_keys("test")
+        browser.find_element_by_css_selector(Common.user_login.login_button['css']).click()
         # Waiting for the payment registration form to be displayed
-        WebDriverWait(browser, 5).until(EC.visibility_of_element_located((By.ID, "payment-new")))
+        browser.find_elements_by_css_selector(User.paymnet_form.it['css'])
+
 
 if __name__ == "__main__":
     unittest.main()
