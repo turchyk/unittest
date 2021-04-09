a90c9d493fe4189886dc391325a6dbff467b70d7
Zakhar Turchyk <turchikz20@gmail.com>
2021-04-09 14:52:04 +0300

    Refactoring the last problematic MainPage method

--- tests/test_one.py
+++ tests/test_one.py
@@ -17,7 +17,8 @@ class TestCase(unittest.TestCase):
     def test_add_to_wish_list(self):
         browser = self.driver
         browser.get('http://opencart/')
-        product_name = MainPage(browser).click_featured_product(1)
+        product_name = MainPage(browser).get_featured_product_name(1)
+        MainPage(browser).click_featured_product(1)
         ProductPage(browser).add_to_wishlist()
         Alert(browser).click_login()
         UserPage(browser).login_user(email="turchikz@gmail.com", password="0000")
@@ -27,7 +28,8 @@ class TestCase(unittest.TestCase):
     def test_add_to_cart(self):
         browser = self.driver
         browser.get('http://opencart/')
-        product_name = MainPage(browser).click_featured_product(1)
+        product_name = MainPage(browser).get_featured_product_name(1)
+        MainPage(browser).click_featured_product(1)
         ProductPage(browser).add_to_cart()
         Alert(browser).click_to_cart()
         CartPage(browser).verify_product(product_name)
