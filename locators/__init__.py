44daf33a12ad8d6356e9610506322a7c00d4aa97
Zakhar Turchyk <turchikz20@gmail.com>
2021-04-09 14:37:35 +0300

    Continue porting methods to PO

--- locators/__init__.py
+++ locators/__init__.py
@@ -3,3 +3,4 @@ from .Main import Main
 from .Product import Product
 from .Cart import Cart
 from .User import User
+from .Alert import Alert
--- tests/test_one.py
+++ tests/test_one.py
@@ -1,7 +1,8 @@
 import unittest
 from selenium import webdriver
-from locators import Common, Product, Cart, User
-from page_objects import MainPage, UserPage
+from locators import Cart, User
+from page_objects import MainPage, UserPage, ProductPage
+from page_objects.common import Alert
 
 
 class TestCase(unittest.TestCase):
@@ -17,13 +18,9 @@ class TestCase(unittest.TestCase):
     def test_add_to_wish_list(self):
         browser = self.driver
         browser.get('http://opencart/')
-        # Click on the first element in the block featured
         product_name = MainPage(browser).click_featured_product(1)
-        # Click on the button on the Product page
-        browser.find_element_by_css_selector(Product.add_to_wishlist['css']).click()
-        # Click on the link in the alert-success block
-        browser.find_element_by_css_selector(Common.alert.success.login['css']).click()
-        # Login from the user authorization form
+        ProductPage(browser).add_to_wishlist()
+        Alert(browser).click_login()
         UserPage(browser).login_user(email="turchikz@gmail.com", password="0000")
         # Go to favorites section
         browser.find_element_by_css_selector(User.right_menu.wish_list['css']).click()
@@ -33,12 +30,9 @@ class TestCase(unittest.TestCase):
     def test_add_to_cart(self):
         browser = self.driver
         browser.get('http://opencart/')
-        # Click on the first element in the block featured
         product_name = MainPage(browser).click_featured_product(1)
-        # Click on the button on the Product page
-        browser.find_element_by_css_selector(Product.add_to_cart['css']).click()
-        # Click on the link in the alert-success block
-        browser.find_element_by_css_selector(Common.alert.success.to_cart['css']).click()
+        ProductPage(browser).add_to_cart()
+        Alert(browser).click_to_cart()
         # Checking the link with the text of the selected product
         browser.find_element_by_link_text(product_name)
         # Click on the Checkout button on the cart page
